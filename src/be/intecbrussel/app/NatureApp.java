49820caf6c69f75d09146d94c23b37d1df45e37a
MickVandenDooren <mickvdd@gmail.com>
2021-08-25 19:41:25 +0200

    2nd commit

--- src/be/intecbrussel/app/NatureApp.java
+++ src/be/intecbrussel/app/NatureApp.java
@@ -12,6 +12,8 @@ public class NatureApp {
 
         ForestNotebook notebook = new ForestNotebook();
 
+        //Adding 5 plants (at least 1 in each category)
+
         Flower plant1 = new Flower("rose");
         notebook.addPlant(plant1);
         plant1.setHeight(10);
@@ -30,6 +32,11 @@ public class NatureApp {
         plant4.setFruit("raspberry");
         notebook.addPlant(plant4);
 
+        Tree plant5 = new Tree("oak",750);
+        notebook.addPlant(plant5);
+
+        //Adding 9 animals (3 for each category)
+
         Herbivore animal1 = new Herbivore("sheep",30,80,100);
         notebook.addAnimal(animal1);
 
@@ -44,28 +51,52 @@ public class NatureApp {
         notebook.addAnimal(animal4);
 
         Herbivore animal5 = new Herbivore("deer",60,200,150);
-        Set<Plant> deerDiet = new HashSet<>();
-        animal5.setPlantDiet(deerDiet);
-        animal5.addPlantToDiet(plant2);
         notebook.addAnimal(animal5);
 
+        Carnivore animal6 = new Carnivore("owl",4,50,170);
+        animal6.setMaxFoodSize(10);
+        notebook.addAnimal(animal6);
+
+        Carnivore animal7 = new Carnivore("black bear",110,150,230);
+        notebook.addAnimal(animal7);
+
+        Omnivore animal8 = new Omnivore("rat",0.5,6,10);
+        notebook.addAnimal(animal8);
+
+        Omnivore animal9 = new Omnivore("swine",50,100,120);
+        notebook.addAnimal(animal9);
+
+        //Adding herbivore diet to herbivore and omnivore animals
+        Set<Plant> herbivoreDiet = new HashSet<>();
+
+        herbivoreDiet.add(plant1);
+        herbivoreDiet.add(plant2);
+        herbivoreDiet.add(plant3);
+        herbivoreDiet.add(plant4);
+
+        animal1.setPlantDiet(herbivoreDiet);
+        animal3.setPlantDiet(herbivoreDiet);
+        animal4.setPlantDiet(herbivoreDiet);
+        animal9.setPlantDiet(herbivoreDiet);
+
+        //print out notebook
         notebook.printNotebook();
 
+        //sort animals
         notebook.sortAnimalsByName();
         notebook.sortPlantsByName();
 
         //Check if duplicate-checks work
-        Flower plant5 = new Flower("rose");
-        notebook.addPlant(plant5);
+        Flower plant6 = new Flower("rose");
+        notebook.addPlant(plant6);
 
-        Herbivore animal6 = new Herbivore("rabbit",4,21,40);
-        notebook.addAnimal(animal6);
+        Herbivore animal10 = new Herbivore("rabbit",4,21,40);
+        notebook.addAnimal(animal10);
 
         //Split output for easy recognition
         System.out.println("///////////////////");
+
+        //Print sorted notebook
         notebook.printNotebook();
-
-
-
     }
 }
